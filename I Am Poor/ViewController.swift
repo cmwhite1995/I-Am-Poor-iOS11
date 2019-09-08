e5feab8b7714724fcc49c71bc713bbe2c5d2f4b2
christopher white <cmwhite1995@gmail.com>
2019-09-08 11:29:30 -0400

    increment by 1 on push

--- I Am Poor/ViewController.swift
+++ I Am Poor/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    @IBOutlet weak var money: UILabel!
+    @IBAction func Give_Money(_ sender: Any) {
+        count += 1
+        money.text = "my money: " + String(count)
+        print(count)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
